eaf260a60aaa8165a9e99bc167d5dc0b1e16cfc3
Sujon Office <>
2025-10-18 10:29:05 +0600

    dashboard dynamic - done

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,26 +2,62 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Brand;
+use App\Models\Orderdetail;
+use App\Models\Product;
+use App\Models\Review;
+use App\Models\Slider;
 use Illuminate\Http\Request;
 use Auth;
 class DashboardController extends Controller
-{   
+{
     public function __construct()
     {
         $this->middleware('auth_check');
     }
     public function Dashboard()
     {
-    	try
-    	{
-    		return view('layouts.app');
-    	}catch(Exception $e){
-                  
+    	try {
+            // Sliders
+            $totalSliders = Slider::count();
+
+            // Products
+            $totalProducts = Product::where('status', 'Active')->count();
+
+            // Reviews
+            $totalReviews = Review::count();
+
+            // Units (sub_domain_id ছাড়া)
+            $totalBrands = Brand::where('status', 'Active')->count();
+
+            // Orders (total, today, monthly, yearly)
+            $totalOrders = OrderDetail::count();
+
+            $todayOrders = OrderDetail::whereDate('created_at', now())->count();
+
+            $monthlyOrders = OrderDetail::whereMonth('created_at', now()->month)
+                ->whereYear('created_at', now()->year)
+                ->count();
+
+            $yearlyOrders = OrderDetail::whereYear('created_at', now()->year)->count();
+
+    		return view('layouts.app', compact(
+                'totalSliders',
+                'totalProducts',
+                'totalReviews',
+                'totalBrands',
+                'totalOrders',
+                'todayOrders',
+                'monthlyOrders',
+                'yearlyOrders'
+            ));
+    	} catch(Exception $e) {
+
                 $message = $e->getMessage();
-      
-                $code = $e->getCode();       
-      
-                $string = $e->__toString();       
+
+                $code = $e->getCode();
+
+                $string = $e->__toString();
                 return response()->json(['message'=>$message, 'execption_code'=>$code, 'execption_string'=>$string]);
                 exit;
         }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,96 +1,139 @@
 @extends('admin_master')
 @section('content')
-<!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Dashboard</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard v1</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Dashboard</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Dashboard</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
     </div>
     <!-- /.content-header -->
 
-     <!-- Main content -->
+    <!-- Main content -->
     <section class="content">
-      <div class="container-fluid">
-        <!-- Small boxes (Stat box) -->
-        <div class="row">
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-info">
-              <div class="inner">
-                <h3>150</h3>
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3>{{$totalSliders ?? 0}}</h3>
 
-                <p>New Orders</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-bag"></i>
-              </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-success">
-              <div class="inner">
-                <h3>53<sup style="font-size: 20px">%</sup></h3>
+                            <p>Total Sliders</p>
+                        </div>
 
-                <p>Bounce Rate</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-stats-bars"></i>
-              </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-warning">
-              <div class="inner">
-                <h3>44</h3>
 
-                <p>User Registrations</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-person-add"></i>
-              </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-danger">
-              <div class="inner">
-                <h3>65</h3>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3>{{$totalProducts ?? 0}}</h3>
 
-                <p>Unique Visitors</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-pie-graph"></i>
-              </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                            <p>Total Products</p>
+                        </div>
+
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-warning">
+                        <div class="inner">
+                            <h3>{{ $totalBrands ?? 0 }}</h3>
+
+                            <p>Total Brands</p>
+                        </div>
+
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-danger">
+                        <div class="inner">
+                            <h3>{{$totalReviews ?? 0}}</h3>
+
+                            <p>Total Reviews</p>
+                        </div>
+
+                    </div>
+                </div>
+                <!-- ./col -->
+
+
+
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3>{{$totalOrders ?? 0}}</h3>
+
+                            <p>Total Orders</p>
+                        </div>
+
+
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3>{{$todayOrders ?? 0}}</h3>
+
+                            <p>Today Orders</p>
+                        </div>
+
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-warning">
+                        <div class="inner">
+                            <h3>{{$monthlyOrders ?? 0}}</h3>
+
+                            <p>This month Orders</p>
+                        </div>
+
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-danger">
+                        <div class="inner">
+                            <h3>{{$yearlyOrders ?? 0}}</h3>
+
+                            <p>This year orders</p>
+                        </div>
+
+                    </div>
+                </div>
+                <!-- ./col -->
+
+
             </div>
-          </div>
-          <!-- ./col -->
-        </div>
-        <!-- /.row -->
-       
-      </div><!-- /.container-fluid -->
+            <!-- /.row -->
+
+        </div><!-- /.container-fluid -->
     </section>
     <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+</div>
+    <!-- /.content-wrapper -->
 @endsection
